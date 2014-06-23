662ef7c9eb8063e9b5c96b28d49cd74b9730edc0
Tom Prince <tom.prince@ualberta.net>
2014-06-22 23:34:51 -0600

    Add documentation.

--- nomenclature/syscalls.py
+++ nomenclature/syscalls.py
@@ -1,3 +1,9 @@
+# Copyright Hybrid Logic Ltd.  See LICENSE file for details.
+
+"""
+Syscalls to manipulate linux namespaces.
+"""
+
 import os
 
 from ._lib import lib, ffi
@@ -7,13 +13,27 @@ def _check_error(errno):
         raise OSError(errno, os.strerror(errno))
 
 def unshare(flags):
+    """
+    Disassociate parts of the process execution context.
+
+    :param flags int: A bitmask that specifies which parts of the execution
+        context should be unshared.
+    """
     res = lib.unshare(flags)
     if res != 0:
         _check_error(ffi.errno)
 
 
-def setns(fd, flags):
-    res = lib.setns(fd, flags)
+def setns(fd, nstype):
+    """
+    Reassociate thread with a namespace
+
+    :param fd int: The file descriptor referreing to one of the namespace
+        entries in a :directory::`/proc/<pid>/ns/` directory.
+    :param nstype int: The type of namespace the calling thread should be
+        reasscoiated with.
+    """
+    res = lib.setns(fd, nstype)
     if res != 0:
         _check_error(ffi.errno)
         
