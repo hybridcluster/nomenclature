61f4f5ff9dcc3c5fa0be9e7b6e2330c654b5d107
Tom Prince <tom.prince@ualberta.net>
2014-06-24 02:02:57 -0600

    Use _GNU_SOURCE.

--- nomenclature/_lib.py
+++ nomenclature/_lib.py
@@ -4,8 +4,10 @@ from cffi import FFI
 
 ffi = FFI()
 ffi.cdef(pkgutil.get_data('nomenclature', 'c/cdef.h').decode('ascii'))
-lib = ffi.verify(pkgutil.get_data('nomenclature', 'c/verify.h').decode('ascii'),
-ext_package='nomenclature',
-)
+lib = ffi.verify(
+    pkgutil.get_data('nomenclature', 'c/verify.h').decode('ascii'),
+    ext_package='nomenclature',
+    extra_compile_args=['-D_GNU_SOURCE']
+    )
 
 __all__ = ('lib', 'ffi')
